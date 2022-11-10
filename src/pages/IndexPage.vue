884ce9ff2d2a2739faa7fa09a2e719d62d999440
Poliana Souza <polysantos88@gmail.com>
2022-11-09 22:21:52 -0300

    Criação da página sobre - incompleto

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,10 +2,12 @@
   <q-page padding>
     <destaques />
     <categorias-lista />
+    <sobre />
   </q-page>
 </template>
 
 <script setup>
 import Destaques from "../components/Destaques/Destaques.vue";
 import CategoriasLista from "../components/Categorias/CategoriasLista.vue";
+import Sobre from "../components/Sobre/Sobre.vue";
 </script>
