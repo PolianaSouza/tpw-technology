dfc328a7611624e6181b09bea5bbf99ff2d16ad3
Poliana Souza <polysantos88@gmail.com>
2022-11-09 21:04:26 -0300

    Inclusão do form para contato e ajustes no layout da página.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,15 @@
 <template>
   <q-layout view="hHh lpR fff">
-    <q-header bordered class="bg-primary text-white">
+    <q-header bordered reveal class="bg-primary text-white">
       <q-toolbar class="text-center text-h5 flex justify-center items-center">
         <q-avatar class="q-mr-md">
           <q-icon name="code" size="40px" />
         </q-avatar>
         TPW Technology
       </q-toolbar>
-      <div class="row">
+      <div class="row justify-between q-mx-sm">
         <q-tabs
-          class="col-sm-8 col-md-8 col-lg-8"
+          class="col-xs-12 col-sm-6 col-md-3 col-lg-3 flex flex-center"
           inline-label
           mobile-arrows
           outside-arrows
@@ -17,10 +17,10 @@
           <q-route-tab to="/" label="Home" />
           <q-route-tab to="/categorias" label="Categorias" />
           <q-route-tab to="/publicacoes" label="Publicações" />
-          <q-route-tab to="/sobre" label="Sobre nós" />
-          <q-route-tab to="/contato" label="Contate-nos" />
+          <!-- <q-route-tab to="/sobre" label="Sobre nós" />
+          <q-route-tab to="/contato" label="Contate-nos" /> -->
         </q-tabs>
-        <search class="col-xs-11 col-sm-4 col-md-4 col-lg-4" />
+        <search class="col-xs-12 col-sm-6 col-md-4 col-lg-4" />
       </div>
     </q-header>
 
@@ -29,9 +29,10 @@
     </q-page-container>
 
     <q-footer class="bg-primary text-white">
+      <footer-informacoes />
       <q-toolbar>
         <q-toolbar-title class="row flex justify-center q-gutter-md">
-          <div>Developed by TPW Technology @2022</div>
+          <div class="text-footer">Developed by TPW Technology @2022</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-footer>
@@ -40,4 +41,10 @@
 
 <script setup>
 import Search from "../components/Search/Search.vue";
+import FooterInformacoes from "../components/Footer/FooterInformacoes.vue";
 </script>
+<style scoped>
+.text-footer {
+  font-size: 0.6em;
+}
+</style>
